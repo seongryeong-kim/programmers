ab4bf69cf154f73062cb4b626989ce86bfccab17
seongryeong-kim <kimsr5383@gmail.com>
2024-07-23 07:05:13 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        if(angle==180){//평각 4
+            answer=4;
+        } else if(angle>90 && angle<180){ //둔각 3
+            answer=3;
+        } else if(angle>0 && angle<90){ //예각 1
+            answer=1;
+        } else { //직각 2
+            answer=2;
+        }
+        return answer;
+    }
+}
